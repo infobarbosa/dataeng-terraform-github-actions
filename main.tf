25102b21b5d7d275ebbb3c660a4d7c6d8e178a14
infobarbosa <infobarbosa@yahoo.com.br>
2024-09-08 15:06:33 -0300

    Refactor AWS S3 bucket name in main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "dataeng_modulo_7_bucket" {
-  bucket = "infobarbosa-asdfasdfasdfasdf-dataeng-modulo-7"
+resource "aws_s3_bucket" "dataeng_bucket" {
+  bucket = "dataeng-modulo-7-${random_string.suffix.result}"
 }
 
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
